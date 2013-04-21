a0be55c028f285b80a85da3256ed99c36d4b8471
Troy Conrad Therrien <troyth@gmail.com>
2013-04-20 22:37:09 -0400

    terms fragments implemented

--- server/projects/air/public/javascripts/main.js
+++ server/projects/air/public/javascripts/main.js
@@ -105,6 +105,57 @@ function($, Alchemy, TAXONOMIES, init, _, Backbone) {
                         Alchemy.getTumblrPost(path_array[6]);
                     });
                 break; 
+                case 'fragments':
+                    //set variables from url
+                    var term_val = url_array.pop().replace(/%20/g," ");
+                    var fragment_val = url_array.pop().replace(/%20/g," ");
+                    url_array.pop();//remove 'fragments'
+                    var taxonomy = url_array.pop().replace(/%20/g," ");
+
+                    //populate dropdowns
+                    for(var i = 0; i < TAXONOMIES[ taxonomy ].length; i++){
+                        $('#sub-nav #terms-nav').append('<option class="menu-option" value="'+TAXONOMIES[ taxonomy ][i]+'">'+TAXONOMIES[ taxonomy ][i]+'</option>');
+                    }
+
+                    //set dropdown values based on current url
+                    $('#sub-nav #terms-nav').val( term_val );
+                    $('#sub-nav #fragments-nav').val( fragment_val );
+
+                    //render out fragments
+                    switch(fragment_val){
+                        case 'quotes':
+                            Alchemy.renderFragmentQuotes(term_val);
+                            break;
+                        case 'images':
+                            Alchemy.renderFragmentImages(term_val);
+                            break;
+                        case 'titles':
+                            Alchemy.renderFragmentTitles(term_val);
+                            break;
+                    }
+
+                    //trigger re-render with Backbone on change event
+                    $('#sub-nav #fragments-nav, #sub-nav #terms-nav').change(function() {
+                        console.log('change event');
+
+                        var fragment = $('#sub-nav #fragments-nav').val();
+                        var tag = $('#sub-nav #terms-nav').val();
+
+                        console.log('fragment: '+ fragment+ ' tag: '+ tag);
+
+                        switch(fragment){
+                            case 'quotes':
+                                Alchemy.renderFragmentQuotes(tag);
+                                break;
+                            case 'images':
+                                Alchemy.renderFragmentImages(tag);
+                                break;
+                            case 'titles':
+                                Alchemy.renderFragmentTitles(tag);
+                                break;
+                        }
+                    });
+                    break;
             }
             break;
         case 'insights':
