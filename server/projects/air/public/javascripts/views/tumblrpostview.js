3ecdf81c151190e4e94e910f66becc450e1622a7
Troy Conrad Therrien <troyth@gmail.com>
2013-05-11 19:27:56 -0400

    can now add fragment images directly from tumblr post

--- server/projects/air/public/javascripts/views/tumblrpostview.js
+++ server/projects/air/public/javascripts/views/tumblrpostview.js
@@ -1,17 +1,69 @@
 define([
 	'backbone',
-	'text!../../../views/templates/tumblrpost.html'
+	'text!../../../views/templates/tumblrpost.html',
+	'taxonomies',
+	'models/fragment'
 ],
-function(Backbone, template) {
+function(Backbone, template, TAXONOMIES, Fragment) {
 
 	var TumblrPostView = Backbone.View.extend({
 	    className: 'tumblrpost',
 		template: template,
+		events: {
+	    	'mouseenter .insight-menu-icon': 'showInsightsMenu'
+	    },
 		/*
 		initialize: function(){
 			_.bind(this.model, 'change', render);
 		},
 		*/
+
+		showInsightsMenu: function(event){
+	    	var that = this;
+	    	var $menu = $('#insights-menu').clone();
+	    	$menu.attr('id', 'insights-menu-live').show();
+
+	    	var $media = $(event.target).closest('.tumblr-post-media');
+
+	    	$media.append($menu);
+
+	    	$menu.bind('mouseleave', function(){
+	    		$(this).remove();
+	    	});
+
+	    	$menu.find('li').bind('click', function(){
+
+	    		var content = $media.find('img').attr('src');
+
+	    		var tags = [];
+	    		$media.closest('#overlay-post').find('.post-tags span').each(function(i){
+	    			tags.push($(this).text());
+	    		});
+
+	    		var category = TAXONOMIES.isCategory(tags);
+
+	    		console.log('category: ');
+	    		console.log(category);
+
+	    		var fragment = new Fragment({
+	    			type: 'image',
+	    			content: content,
+	    			element: that.model.get('id'),
+	    			tags: tags,
+	    			category: category,
+	    			insight_id: $(this).attr('id').substr(8),
+	    			post_url: that.model.get('post_url')
+	    		});
+	    		
+	    		fragment.save({}, {
+	    			success: function(model, response){
+	    				console.dir(model);
+	    				console.dir(response);
+	    			}
+	    		});
+
+	    	});
+	    },
 
 		render: function(){
 			//use Underscore template, pass it the attributes from this model
@@ -24,6 +76,9 @@ function(Backbone, template) {
 
 			var content = _.template(this.template, attr);
 			$(this.el).html(content);
+
+			//set media wrapper to size of image inside
+			$('.tumblr-post-media').each(function(){ $(this).width( $(this).find('img, iframe, object').attr('width') )  });
 
 			// return ```this``` so calls can be chained.
 			return true;
